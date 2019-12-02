56b4c57e396ce4ac95f100b8fd9edb148f637bf5
amhammett <am.hammett@gmail.com>
2019-12-02 15:15:30 +1100

    now with part 2 logic. functions should be renamed

--- src/day1-fuel-required.ts
+++ src/day1-fuel-required.ts
@@ -1,15 +1,26 @@
+const part1 = (input:number) => {
+  let fuel = Math.floor(input/3) - 2;
+
+  if (fuel < 0) {
+    fuel = 0;
+  }
+
+  return fuel;
+};
+
+const part2 = (input: number) => {
+  let fuel = part1(input);
+  let total = fuel;
+
+  while (fuel > 0) {
+    fuel = part1(fuel);
+    total += fuel;
+  }
+
+  return total;
+};
 
 module.exports = {
-  part1: (input: number) => {
-    let fuel = Math.floor(input/3) - 2;
-
-    if (fuel < 0){
-      fuel = 0;
-    }
-
-    return fuel;
-  },
-  part2: (input: number) => {
-    
-  }
+  part1,
+  part2,
 };
