d3b194c19bd8d76b338f879a29d9f01bc8e3f8d1
amhammett <am.hammett@gmail.com>
2019-12-02 15:15:52 +1100

    now with part2 logic

--- src/day1-input.ts
+++ src/day1-input.ts
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
 const day1 = require('../src/day1-fuel-required');
 
-let fuelTotal = 0;
-let massLines = fs.readFileSync(process.cwd() + '/input/day1.txt').toString().split('\n');
+// tslint:disable-next-line:no-any
+let day1Process = (processFunction: any, path: string) => {
+  const fs = require('fs');
+  const inputFilePath = process.cwd() + path;
 
-for (let i = 0; i< massLines.length; i++) {
-  fuelTotal += day1.fuelRequired(massLines[i]);
-}
+  let fuelTotal = 0;
+  const massLines = fs.readFileSync(inputFilePath).toString().split('\n');
 
-console.log(fuelTotal);
+  for (let i = 0; i< massLines.length; i++) {
+    fuelTotal += processFunction(massLines[i]);
+  }
+
+  return fuelTotal;
+};
+
+console.log('day1 part1: ' + day1Process(day1.part1, '/input/day1.part1.txt'));
+console.log('day1 part2: ' + day1Process(day1.part2, '/input/day1.part2.txt'));
